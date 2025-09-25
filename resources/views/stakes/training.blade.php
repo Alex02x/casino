b8a6792bd08ca60d993b27baa93eb7eff5ef581b
degrando <cooplodoop6@gmail.com>
2025-09-26 01:02:04 +0300

    first

--- resources/views/stakes/training.blade.php
+++ resources/views/stakes/training.blade.php
@@ -5,6 +5,7 @@
             '3-plus' => '3+ стэков',
             'ultra' => 'Ultra Mode (20+ стэков)',
             default => 'Неизвестный режим'
+            }
         }}</h2>
         <p class="text-gray-600 dark:text-gray-300 mb-6">Интерфейс тренировки будет здесь.</p>
         <a href="{{ route('stakes.mode') }}" class="text-blue-600 hover:underline">← Вернуться к выбору режима</a>

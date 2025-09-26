25ac6226ca374d7d889a314bf8b54478654cac1a
degrando <cooplodoop6@gmail.com>
2025-09-26 03:10:02 +0300

    first

--- resources/views/livewire/roulette-page.blade.php
+++ resources/views/livewire/roulette-page.blade.php
@@ -185,13 +185,13 @@
                     @foreach ($this->getTrainingPositions() as $bet)
                         @if($bet['chips'] != 0)
                             <div
-                                class="absolute flex items-center justify-center text-4xl font-bold text-white"
+                                class="absolute flex items-center justify-center text-2xl font-bold text-white"
                                 style="
                                 left: {{ $bet['x'] }}%;
                                 top: {{ $bet['y'] }}%;
                                 transform: translate(-50%, -50%);
-                                width: 96px;
-                                height: 96px;
+                                width: 48px;
+                                height: 48px;
                             "
                             >
                                 <div class="w-full h-full rounded-full bg-blue-600 flex items-center justify-center shadow-md">
